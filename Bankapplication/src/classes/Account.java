754e5835bfbdd43cf32969efa3469a2c4bc66631
herzo1 <o.herzig95@gmail.com>
2019-10-04 11:23:10 +0200

    Added simple methodes to account, not finished marked with TODO

--- Bankapplication/src/classes/Account.java
+++ Bankapplication/src/classes/Account.java
@@ -14,4 +14,32 @@ public class Account {
 		this.pin = pin;
 		this.balance = balance;
 	}
+	
+	public boolean checkPin(String pin) {
+		return this.pin.equals(pin);
+	}
+	
+	public boolean deposit(double amount) {
+		// TODO: implement this method
+		return false;
+	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	public int getNr() {
+		return this.nr;
+	}
+	
+	public boolean withdraw(double amount) {
+		// TODO: implement this method
+		return false;
+	}
+	
+	public String toString() {
+		// TODO: implement this method
+		return "";
+	}
+	
 }

6243cc2000e5361ec77a4563a36eb6b0a6300569
herzo1 <o.herzig95@gmail.com>
2019-11-22 11:52:35 +0100

    Refactoring

--- Bankapplication/src/bankapp/Main.java
+++ Bankapplication/src/bankapp/Main.java
@@ -1,7 +1,9 @@
 package bankapp;
 
 import bankapp.atm.ATM;
+import bankapp.bank.Account;
 import bankapp.bank.Bank;
+import bankapp.bank.Customer;
 import bankapp.cli.BankCLI;
 
 public class Main {
@@ -9,15 +11,13 @@ public class Main {
 	private static final String BANK_NAME = "UBS";
 	
 	public static void main(String[] args) {
-
+		
 		Bank ubs = new Bank(BANK_NAME);
 
 		new ATM(ubs);
 
 		BankCLI ubsCli = new BankCLI(ubs);
 		ubsCli.run();
-		
-
 		
 	}
 }

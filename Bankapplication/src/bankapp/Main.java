260da43a7876f3bdd468ec01d9e8758d7f95a415
herzo1 <o.herzig95@gmail.com>
2019-11-29 08:38:43 +0100

    Insert a close() call for the atm-machine

--- Bankapplication/src/bankapp/Main.java
+++ Bankapplication/src/bankapp/Main.java
@@ -9,13 +9,10 @@ public class Main {
 	private static final String BANK_NAME = "UBS";
 	
 	public static void main(String[] args) {
-		
 		Bank ubs = new Bank(BANK_NAME);
-
-		new ATM(ubs);
-
+		ATM atm = new ATM(ubs);
 		BankCLI ubsCli = new BankCLI(ubs);
 		ubsCli.run();
-		
+		atm.close();
 	}
 }

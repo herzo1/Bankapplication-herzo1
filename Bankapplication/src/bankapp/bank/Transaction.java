db5e3afffa644d9657d9bfef9da6eb1d6b843bce
herzo1 <o.herzig95@gmail.com>
2019-12-06 08:50:43 +0100

    Made vars final to show it's an immutable class

--- Bankapplication/src/bankapp/bank/Transaction.java
+++ Bankapplication/src/bankapp/bank/Transaction.java
@@ -3,10 +3,10 @@ package bankapp.bank;
 import java.time.LocalDate;
 
 public class Transaction {
-	private double amount;
-	private double balance;
-	private LocalDate valuta;
-	
+	private final double amount;
+	private final double balance;
+	private final LocalDate valuta;
+
 	public Transaction(double amount, double balance) {
 		this.amount = amount;
 		this.balance = balance;

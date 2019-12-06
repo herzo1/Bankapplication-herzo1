4422635fd44614f6013e2c376f7258d1838a98be
herzo1 <o.herzig95@gmail.com>
2019-12-06 19:06:45 +0100

    changed the Main-class so it can run javaFX

--- Bankapplication/src/bankapp/Main.java
+++ Bankapplication/src/bankapp/Main.java
@@ -1,33 +1,58 @@
 package bankapp;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import bankapp.atm.ATM;
 import bankapp.bank.Account;
 import bankapp.bank.Bank;
 import bankapp.bank.Customer;
 import bankapp.cli.BankCLI;
+import bankapp.ebanking.LoginPane;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends Application {
+	private static final String LOGIN_STAGE_TITLE = "E-Banking";
 	
-	private static final String BANK_NAME = "UBS";
+	private static final String BANK_NAME = "EASYBank";
 	private static final String PIN = "123";
 	private static List<Account> accList = new ArrayList<>();
-	
-	
-	public static void main(String[] args) {
-		Bank ubs = new Bank(BANK_NAME);
 
-		Customer cust1 = ubs.registerCustomer("Oliver", PIN);
+	@Override
+	public void init() throws Exception{
+		Bank easyBank = new Bank(BANK_NAME);
+
+		Customer cust1 = easyBank.registerCustomer("Oliver", PIN);
 		for(int i=0; i<5; i++) {
-			accList.add(ubs.openPersonalAccount(cust1, PIN));
+			accList.add(easyBank.openPersonalAccount(cust1, PIN));
 		}
-		ubs.deposit(accList.get(1).getNr(), 1000);
+		easyBank.deposit(accList.get(1).getNr(), 1000);
 		System.out.println(cust1);
-		ATM atm = new ATM(ubs);
-		BankCLI ubsCli = new BankCLI(ubs);
-		ubsCli.run();
-		atm.close();
+		ATM atm = new ATM(easyBank);
+		BankCLI bankCli = new BankCLI(easyBank);
+		// bankCli.run();
 	}
+
+	@Override
+	public void start(Stage stage) throws Exception{
+		BorderPane root = new LoginPane();
+
+		stage.setScene(new Scene(root, 400, 400));
+		stage.setTitle(LOGIN_STAGE_TITLE);
+		stage.show();
+	}
+
+	@Override
+	public void stop() throws Exception{
+		// TODO: why doesn't does atm.close() function
+	    // atm.close();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
